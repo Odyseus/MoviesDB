98a9713c93a1050b1e8847e82d7301ee3a5c909c
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:39:21 -0300

    Added README and updated __init__.py module.

--- AppData/MoviesDBApp/__init__.py
+++ AppData/MoviesDBApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Unstable)"
 __appname__ = "Movies DB"
 __appdescription__ = "A CLI utility to create a movies data base on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.333"
+
+
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Movies DB ([documentation](https://pythoncliapplications.gitlab.io/CLIApplicationsManager/includes/MoviesDB/index.html))
+
+A CLI utility to create a movies data base on GNU/Linux.
