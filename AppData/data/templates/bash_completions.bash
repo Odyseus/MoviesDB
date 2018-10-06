529aa5364b511e55fc30e02396b5c2c10c114087
Odyseus <venusyadonis@gmail.com>
2018-10-06 10:19:44 -0300

    Updated Bash completions script to be less dependent on Bash functions that may or may not exist.

--- AppData/data/templates/bash_completions.bash
+++ AppData/data/templates/bash_completions.bash
@@ -3,13 +3,13 @@
 # It would have been impossible to create this without the following post on Stack Exchange!!!
 # https://unix.stackexchange.com/a/55622
 
-_have {executable_name} &&
+type "{executable_name}" &> /dev/null &&
 _decide_nospace_{current_date}(){
     if [[ ${1} == "--"*"=" ]] ; then
-        compopt -o nospace
+        type "compopt" &> /dev/null && compopt -o nospace
     fi
 } &&
-__movies_db_app_{current_date}(){
+__movies_db_cli_{current_date}(){
     local cur prev cmd
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
@@ -17,7 +17,7 @@ __movies_db_app_{current_date}(){
 
     # Completion of commands and "first level options.
     if [[ $COMP_CWORD == 1 ]]; then
-        COMPREPLY=( $(compgen -W "movies server generate -h --help --version" -- "${cur}") )
+        COMPREPLY=( $(compgen -W "movies server generate -h --help --manual --version" -- "${cur}") )
         return 0
     fi
 
@@ -37,4 +37,4 @@ __movies_db_app_{current_date}(){
         ;;
     esac
 } &&
-complete -F __movies_db_app_{current_date} {executable_name}
+complete -F __movies_db_cli_{current_date} {executable_name}
