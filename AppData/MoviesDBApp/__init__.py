d7f9318d86983d5b771189aff9cfb6db7d2d0cc6
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:29:52 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/MoviesDBApp/__init__.py
+++ AppData/MoviesDBApp/__init__.py
@@ -3,7 +3,7 @@
 
 __status__ = "Unstable"
 __appname__ = "Movies DB"
-__appdescription__ = "A CLI utility to create a movies data base on GNU/Linux."
+__appdescription__ = "CLI utility to create a movies data base on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.333"
 
 
