babf7fc30e27a382d88eb2c3fd4a7028dbc7faee
Odyseus <venusyadonis@gmail.com>
2020-02-14 03:49:49 -0300

    Bump version

--- AppData/MoviesDBApp/__init__.py
+++ AppData/MoviesDBApp/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 __status__ = "Unstable"
 __appname__ = "Movies DB"
 __appdescription__ = "CLI utility to create a movies data base on GNU/Linux."
-__version__ = "2018-09-15 01:19:13.333"
+__version__ = "2020-02-14 03:47:21.876"
 
 
 if __name__ == "__main__":
